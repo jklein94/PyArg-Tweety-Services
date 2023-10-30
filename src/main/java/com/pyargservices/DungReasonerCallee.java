5b78950d220250d5e875c369829f6dc399713810
jklein94 <jklein94@uni-koblenz.de>
2023-10-30 11:59:44 +0100

    initial commit

---
+++ src/main/java/com/pyargservices/DungReasonerCallee.java
@@ -0,0 +1,14 @@
+package com.pyargservices;
+
+import java.util.concurrent.Callable;
+
+public class DungReasonerCallee implements Callable {
+
+    @Override
+    public Object call() throws Exception {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'call'");
+    }
+
+    
+}
---
+++ src/main/java/com/pyargservices/PyargServicesApplication.java
@@ -0,0 +1,13 @@
+package com.pyargservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PyargServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PyargServicesApplication.class, args);
+	}
+
+}
